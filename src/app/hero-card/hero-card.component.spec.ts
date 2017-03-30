c7d7ef29f869b3be125faef70771b0193d3f7aa2
benjongbloedt <bjongbloedt@users.noreply.github.com>
2017-03-30 16:03:20 -0500

    fixing Hero-card tests

--- src/app/hero-card/hero-card.component.spec.ts
+++ src/app/hero-card/hero-card.component.spec.ts
@@ -1,25 +1,54 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Component } from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 import { HeroCardComponent } from './hero-card.component';
+import { HeroService } from '../hero.service';
+
+@Component({
+  template: '<div></div>'
+})
+class DummyComponent {
+}
 
 describe('HeroCardComponent', () => {
-  let component: HeroCardComponent;
-  let fixture: ComponentFixture<HeroCardComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeroCardComponent ]
+      declarations: [HeroCardComponent, DummyComponent],
+      imports: [
+        MaterialModule,
+        RouterTestingModule.withRoutes([
+          { path: 'detail', component: DummyComponent }
+        ])
+      ],
+      providers: [
+        HeroService
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(HeroCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(HeroCardComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Moar', () => {
+    let component: HeroCardComponent;
+    let fixture: ComponentFixture<HeroCardComponent>;
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(HeroCardComponent);
+      fixture.componentInstance.hero = {id: 0, name: 'fred'};
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
   });
 });
