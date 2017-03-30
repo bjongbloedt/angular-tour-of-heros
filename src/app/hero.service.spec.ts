66316ce4d64631c24ac947f729e44f67adc1486e
benjongbloedt <bjongbloedt@users.noreply.github.com>
2017-03-30 15:25:19 -0500

    moving to fixing tests

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -3,21 +3,23 @@ import { MockBackend } from '@angular/http/testing';
 import { HttpModule, Response, ResponseOptions } from '@angular/http';
 import { HeroService } from './hero.service';
 
+import 'rxjs/add/operator/do';
 
-fdescribe('HeroService', () => {
+
+describe('HeroService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         HeroService,
       ],
-      imports: [ HttpModule ]
+      imports: [HttpModule]
     });
   });
 
   describe('#getHeroes', () => {
     it('should be empty', inject([HeroService], (service: HeroService, done) => {
-      service.getHeroes().then(heroes => {
-        expect(heroes).toEqual(['fart']);
+      service.getHeroes().do(heroes => {
+        expect(heroes).toEqual([{ id: 0, name: 'fart' }]);
         done();
       });
     }));
