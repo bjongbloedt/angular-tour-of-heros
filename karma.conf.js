c3fdd3c9b82bac8d785a49d2b413a15379e6b583
benjongbloedt <bjongbloedt@users.noreply.github.com>
2017-04-10 18:53:48 -0500

    updating things for circle ci

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-junit-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -38,8 +39,13 @@ module.exports = function (config) {
       environment: 'dev'
     },
     reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+              ? ['progress', 'coverage-istanbul', 'junit']
+              : ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      outputDir: 'junit/',
+      outputFile: 'test-results.xml',
+      useBrowserName: false
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
